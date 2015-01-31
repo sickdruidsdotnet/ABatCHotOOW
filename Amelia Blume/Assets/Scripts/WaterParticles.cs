817472deefe3fd374affc1ed25b0071cc9e15f77
smarredo <gam3r666@dhcp-210-32.cruznet.ucsc.edu>
2015-01-30 17:31:01 -0800

    Added in water action & soil collection

---
+++ Amelia Blume/Assets/Scripts/WaterParticles.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterParticles : MonoBehaviour {
+	bool rightTrigger;
+	int coolDown;
+	public ParticleSystem water;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rightTrigger = Input.GetKey (KeyCode.JoystickButton7);
+		if (rightTrigger && coolDown > 10)
+		{
+			StartWater ();
+			coolDown = 0;
+		}
+
+		coolDown++;
+	}
+
+	void StartWater()
+	{
+		water.Emit (1);
+	}
+
+	void OnParticleCollision(GameObject other) {
+		if (other.tag == "Soil") {
+			//Debug.Log ("Soil collision");
+			//Add to hydration level of soil later on
+		}
+	}
+}
---
+++ Amelia Blume/Assets/Soil.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Soil : MonoBehaviour {
+	private int HydrationLevel;
+	private bool isPlanted;
+	// Use this for initialization
+	void Start () {
+		HydrationLevel = 0;
+		isPlanted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (GetHydrationLevel ());
+	}
+
+	int GetHydrationLevel(){
+		return HydrationLevel;
+	}
+
+	void ChangeHydrationLevel(int water){
+		HydrationLevel += water;
+	}
+
+	bool CheckIsPlanted(){
+		return isPlanted;
+	}
+
+	void OnParticleCollision(GameObject other) {
+		if (other.tag == "Water") {
+			ChangeHydrationLevel (1);
+		}
+	}
+
+
+
+}
