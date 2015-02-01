ac741088ee4a7cc4117809a7cde5194665755275
smarredo <gam3r666@dhcp-217-14.cruznet.ucsc.edu>
2015-01-31 17:44:40 -0800

    Added Soil functionality

--- Amelia Blume/Assets/Soil.cs
+++ Amelia Blume/Assets/Soil.cs
@@ -4,19 +4,41 @@ using System.Collections;
 public class Soil : MonoBehaviour {
 	private int HydrationLevel;
 	private bool isPlanted;
+	private Vector3 SeedLocation;
+	private int SeedWater;
+	private int SeedType;
+	private int MaxWater;
+	//private Seed plantedSeed;
 	// Use this for initialization
 	void Start () {
 		HydrationLevel = 0;
 		isPlanted = false;
+		MaxWater = 100;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (GetHydrationLevel ());
+		if (CheckIsPlanted()) {
+			ChangeHydrationLevel(-1);
+			//plantedSeed.CollectWater();
+		}
+		//Debug.Log (GetHydrationLevel ());
 	}
 
 	int GetHydrationLevel(){
 		return HydrationLevel;
+	}
+
+	void SetSeedLocation(Vector3 location){
+		SeedLocation = location;
+	}
+
+	Vector3 GetSeedLocation(){
+		return SeedLocation;
+	}
+
+	void CreatePlant(){
+		//
 	}
 
 	void ChangeHydrationLevel(int water){
@@ -28,8 +50,18 @@ public class Soil : MonoBehaviour {
 	}
 
 	void OnParticleCollision(GameObject other) {
-		if (other.tag == "Water") {
+		if (other.tag == "Water" && GetHydrationLevel() < MaxWater) {
 			ChangeHydrationLevel (1);
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Seed" && !CheckIsPlanted()) {
+			//plantedSeed = other.gameObject.GetComponent<Seed>();
+			//if(!plantedSeed.checkIfPlanted()){
+				//plantedSeed.isPlanted = true;
+				//this.isPlanted = true;
+			//}
 		}
 	}
 
