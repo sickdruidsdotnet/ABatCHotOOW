82dcc987fff3403a46729731c6da4461c16aad37
smarredo <gam3r666@dhcp-217-14.cruznet.ucsc.edu>
2015-01-31 19:03:21 -0800

    Merge branch 'master' into feature/water
    Conflicts:
    	Amelia Blume/Amelia Blume.userprefs
    	Amelia Blume/Assets/Animation/Animations/JumpUpHigh.fbm.meta
    	Amelia Blume/Assets/Meshes/Animals/Materials.meta
    	Amelia Blume/Assets/Meshes/Candy/61712_cc_by_electric5auce/CC.meta
    	Amelia Blume/Assets/Meshes/Materials.meta
    	Amelia Blume/ProjectSettings/TagManager.asset

---
+++ Amelia Blume/Assets/Scripts/Seed/VineSeed.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+//This is the base class which is
+//also known as the Parent class.
+public class VineSeed : Seed 
+{
+    
+    // Constructor
+    public VineSeed()
+    {
+        
+        // set hydrationGoal to match VineSeed requirements
+        this.hydrationGoal = 20;
+
+        Debug.Log("VineSeed created");
+        Debug.Log("VineSeed hydrationGoal: " + hydrationGoal);
+    }
+}
