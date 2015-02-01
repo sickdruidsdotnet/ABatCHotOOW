be7cb2019cce51165c069a085f8d9fb2f8050baf
Joe Wise <JosephWise93@gmail.com>
2015-01-31 20:56:11 -0800

    Jump through platforms!

---
+++ Amelia Blume/Assets/Scripts/platformTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.parent.collider.isTrigger = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("OnTriggerEnter called");
+		if (other.name == "Player")
+			transform.parent.collider.isTrigger = false;
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		Debug.Log("OnTriggerExit called");
+		if (other.name == "Player")
+			transform.parent.collider.isTrigger = true;
+	}
+}
