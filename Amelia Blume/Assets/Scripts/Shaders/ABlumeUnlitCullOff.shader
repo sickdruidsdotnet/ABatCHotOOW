8af5b86523236c0518b3edbb380d333c45922b4a
cdilday <cdilday@ucsc.edu>
2015-02-21 11:31:59 -0800

    - Pressing down now lets the player fall through jump through platforms - added a shader that turns backface culling off, use for the player - fixed the camera controller causing an error whenever re-importing the player prefab - move some files to their proper place

---
+++ Amelia Blume/Assets/Scripts/Shaders/ABlumeUnlitCullOff.shader
@@ -0,0 +1,21 @@
+﻿Shader "ABlumeUnlit" {
+    Properties {
+        _Color ("Main Color", Color) = (1,1,1,1)
+        _MainTex ("Base (RGB) Trans (A)", 2D) = "white" {}
+    }
+    Category {
+       Lighting Off
+       ZWrite On
+       Cull Off
+       SubShader {
+            Pass {
+            Blend SrcAlpha OneMinusSrcAlpha
+            Tags {Queue=Transparent}
+               SetTexture [_MainTex] {
+                    constantColor [_Color]
+                    Combine texture * constant, texture * constant 
+                }
+            }
+        } 
+    }
+}
--- Amelia Blume/Assets/Scripts/platformTrigger.cs
+++ Amelia Blume/Assets/Scripts/platformTrigger.cs
@@ -2,23 +2,52 @@
 using System.Collections;
 
 public class platformTrigger : MonoBehaviour {
-	Transform platParent;
+	public Transform platParent;
+	//need the Player controller to tell if crouching
+	public GameObject amelia;
+	public PlayerController aController;
+	bool playerColliding;
+
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		platParent = transform.parent;
+
+		amelia = GameObject.FindGameObjectWithTag ("Player");
+		if (amelia != null) {
+			aController = amelia.GetComponent<PlayerController> ();
+		}
+
+		playerColliding = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log ("input is: " + Input.GetAxis ("Vertical"));
+		if (Input.GetAxis ("Vertical") < (-1f * aController.inputDeadZone)) {
+			Debug.Log ("in if");
+			Physics.IgnoreCollision (amelia.GetComponent<BoxCollider> (), platParent.collider, true);
+			Physics.IgnoreCollision (amelia.GetComponent<CharacterController> (), platParent.collider, true);
+		} else if(!playerColliding) {
+			Physics.IgnoreCollision (amelia.GetComponent<BoxCollider>(), platParent.collider, false);
+			Physics.IgnoreCollision (amelia.GetComponent<CharacterController>(), platParent.collider, false);
+		}
+
 	}
 	
 	void OnTriggerStay(Collider other)
 	{
+		if (other.tag == "Player") {
+			playerColliding = true;
+		}
 		Physics.IgnoreCollision (other, platParent.collider, true);
 	}
 	
 	void OnTriggerExit(Collider other)
 	{
+		if (other.tag == "Player") {
+			playerColliding = false;
+		}
 		Physics.IgnoreCollision (other, platParent.collider, false);
 	}
 }
