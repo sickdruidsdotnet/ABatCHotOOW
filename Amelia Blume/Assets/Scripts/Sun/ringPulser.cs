caba91f9c496a8113faedc22caedc461066c035c
cdilday <cdilday@ucsc.edu>
2015-02-27 13:07:57 -0800

    First pass on sun asset and animation. Will work on it during jam after feedback

---
+++ Amelia Blume/Assets/Scripts/Sun/ringPulser.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ringPulser : MonoBehaviour {
+	
+	Vector3 startScale;
+	float transparency = 0.8f;
+	// Use this for initialization
+	void Start () {
+		startScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transparency <= 0) {
+			transform.localScale = startScale;
+			transparency = 0.8f;
+		} else {
+			transparency -= 0.01f;
+			transform.localScale = new Vector3(transform.localScale.x + 0.13f, 
+			                                   transform.localScale.y, 
+			                                   transform.localScale.z + 0.13f);
+			Color newColor = new Color(renderer.material.color.r, 
+			                           renderer.material.color.g,
+			                           renderer.material.color.b,
+			                           transparency);
+			renderer.material.SetColor("_Color", newColor);
+		}
+		transform.Rotate(new Vector3(0f, 4f, 0f));
+	}
+}
