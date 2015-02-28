47fc067648a8559a41e94fa990af752d08bf9d3a
cdilday <cdilday@ucsc.edu>
2015-02-27 21:28:39 -0800

    Added Rays to the sun asset which randomly rotate and fade in and out at varying intervals

---
+++ Amelia Blume/Assets/SunRay.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunRay : MonoBehaviour {
+	float transparencyMax = 0.5f;
+	float rotationSpeed;
+	Vector3 pivot;
+	public int startLife;
+	public int life = 60;
+	public int lifeThirds;
+	public float transparencySpeed;
+
+	// Use this for initialization
+	void Start () {
+		Color newColor = new Color(renderer.material.color.r, 
+		                           renderer.material.color.g,
+		                           renderer.material.color.b,
+		                           0f);
+		renderer.material.SetColor ("_Color", newColor);
+		pivot = transform.parent.transform.position;
+		life = Random.Range(40, 180);
+		startLife = life;
+		lifeThirds = life/3;
+		transform.RotateAround (pivot, Vector3.back, Random.Range(0, 360));
+		rotationSpeed = Random.Range (1, 3);
+		if (Random.Range (0, 5) > 2)
+			rotationSpeed *= -1;
+		transparencySpeed = (float)transparencyMax / (float)lifeThirds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		life--;
+		if (life <= 0) {
+			Color newColor = new Color (renderer.material.color.r, 
+			                           renderer.material.color.g,
+			                           renderer.material.color.b,
+			                           0f);
+			renderer.material.SetColor ("_Color", newColor);
+			transform.RotateAround (pivot, Vector3.back, Random.Range (0, 360));
+			life = Random.Range (40, 180);
+			startLife = life;
+			lifeThirds = life / 3;
+			transparencySpeed = (float)transparencyMax / (float)lifeThirds;
+			rotationSpeed = Random.Range (1, 3);
+			if (Random.Range (0, 5) > 2)
+				rotationSpeed *= -1;
+		} else if (life > startLife - lifeThirds) {
+			Color newColor = new Color (renderer.material.color.r, 
+			                            renderer.material.color.g,
+			                            renderer.material.color.b,
+			                            renderer.material.color.a + transparencySpeed);
+			renderer.material.SetColor ("_Color", newColor);
+			transform.RotateAround (pivot, Vector3.back, rotationSpeed);
+		} else if (life < startLife - (2 * lifeThirds) ) {
+			Color newColor = new Color (renderer.material.color.r, 
+			                            renderer.material.color.g,
+			                            renderer.material.color.b,
+			                            renderer.material.color.a - transparencySpeed);
+			renderer.material.SetColor ("_Color", newColor);
+			transform.RotateAround (pivot, Vector3.back, rotationSpeed);
+		} else {
+			//rotate as normal;
+			transform.RotateAround (pivot, Vector3.back, rotationSpeed);
+		}
+	}
+}
