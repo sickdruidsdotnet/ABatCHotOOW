dd4e2c58d57c3181fc09975558b31b384de8293d
cdilday <cdilday@ucsc.edu>
2015-02-27 19:33:18 -0800

    Improved ring animation

--- Amelia Blume/Assets/Scripts/Sun/ringPulser.cs
+++ Amelia Blume/Assets/Scripts/Sun/ringPulser.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 
 public class ringPulser : MonoBehaviour {
-	
+	public int offset = 0;
 	Vector3 startScale;
-	float transparency = 0.8f;
+	float progress = 120;
+	float transparency = 1f;
 	// Use this for initialization
 	void Start () {
 		startScale = transform.localScale;
@@ -12,19 +13,54 @@ public class ringPulser : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (transparency <= 0) {
-			transform.localScale = startScale;
-			transparency = 0.8f;
-		} else {
-			transparency -= 0.01f;
-			transform.localScale = new Vector3(transform.localScale.x + 0.13f, 
-			                                   transform.localScale.y, 
-			                                   transform.localScale.z + 0.13f);
+		if (offset > 0){
+			offset--;
+			if(offset == 0)	{
+				Color newColor = new Color(renderer.material.color.r, 
+				                           renderer.material.color.g,
+				                           renderer.material.color.b,
+				                           1f);
+			}
+			else{
+				Color newColor = new Color(renderer.material.color.r, 
+				                           renderer.material.color.g,
+				                           renderer.material.color.b,
+				                           0f);
+				renderer.material.SetColor("_Color", newColor);
+			}
+			return;
+		}
+
+		if (progress >=120 ) {
+			progress = 0;
+			transparency = 1f;
 			Color newColor = new Color(renderer.material.color.r, 
 			                           renderer.material.color.g,
 			                           renderer.material.color.b,
 			                           transparency);
 			renderer.material.SetColor("_Color", newColor);
+			this.GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (0, (float)progress);
+			//transform.localScale = startScale;
+		} else {
+			progress += 1f;
+			Vector3 tempScale = transform.localScale;
+			this.GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (0, (float)progress);
+			/*tempScale = new Vector3(transform.localScale.x + 0.1f, 
+                                   transform.localScale.y + 0.1f, 
+                                   transform.localScale.z + 0.1f);
+			transform.localScale = tempScale;*/
+			if(progress >=50)
+			{
+				transparency -=0.08f;
+				if(transparency < 0)
+					transparency = 0;
+				Color newColor = new Color(renderer.material.color.r, 
+				                           renderer.material.color.g,
+				                           renderer.material.color.b,
+				                           transparency);
+				renderer.material.SetColor("_Color", newColor);
+
+			}
 		}
 		transform.Rotate(new Vector3(0f, 4f, 0f));
 	}
