005f2abf6f9b343aa97bd05d23ead9c52cd02e2e
cdilday <cdilday@ucsc.edu>
2015-03-13 18:37:17 -0700

    Created a blade of grass to test a sway script. also we have a fluff folder

---
+++ Amelia Blume/Assets/PlantSway.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantSway : MonoBehaviour {
+	float progress = 100f;
+	bool swayRight = true;
+	bool adding = true;
+	
+	// Update is called once per frame
+	void Update () {
+		if (adding) {
+			progress++;
+			if (progress >= 50f) {
+				adding = !adding;
+			}
+
+		} else {
+			progress--;
+			if (progress <= 0f) {
+				adding = !adding;
+				swayRight = !swayRight;
+			}
+		}
+
+		if (swayRight) {
+			this.GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (1, (float)progress);
+		} else {
+			this.GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (0, (float)progress);
+		}
+
+	}
+}
