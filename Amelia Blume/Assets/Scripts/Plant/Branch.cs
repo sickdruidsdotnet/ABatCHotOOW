964fce937d33a75dfe37dd4795476b57c81b8315
Propolandante <propolandante@gmail.com>
2015-04-06 13:42:25 -0700

    Branch WIP, pushing changes made in Game Lab so I can continue from home

--- Amelia Blume/Assets/Scripts/Plant/Branch.cs
+++ Amelia Blume/Assets/Scripts/Plant/Branch.cs
@@ -40,8 +40,12 @@ public class Branch
 	public List<Branch> children;
 	public Vector3 startPoint;
 	public Vector3 direction;
-	public float lengthGoal;
 	public float branchMaturity;
+	public float lengthGoal;
+	private float growthRate = 0.05f;
+	private float growthStart;
+	public bool isGrowing = false;
+	private float maxSegLength = 0.2f;
 
 	// "trunk" branch constructor
 	public Branch(Vector3 start, Vector3 dir, float initialRadius = 0.02f, float maxLength = 1.0f, float length = 0)
@@ -56,6 +60,7 @@ public class Branch
 		startPoint = start;
 		direction = dir;
 		lengthGoal = maxLength;
+		growthStart = length;
 
 		skeleton.Add(new BranchNode(initialRadius, length, startPoint, direction));
 	}
@@ -90,10 +95,88 @@ public class Branch
 		startPoint = parent.skeleton[parentNode].getNodeEndPoint();
 		direction = dir;
 		lengthGoal = maxLength;
+		growthStart = length;
 
 		skeleton.Add(new BranchNode(initialRadius, length, startPoint, direction));
 
 		branchMaturity = Mathf.Clamp01(getLength() / lengthGoal);
+	}
+
+	public void Update()
+	{
+		bool wasGrowing = isGrowing;
+		// update vine skeleton structure (such as adding a new segment)
+		if (getTotalLength() < lengthGoal)
+		{
+			grow();
+			isGrowing = true;
+		}
+		else if (wasGrowing)
+		{
+			isGrowing = false;
+		}
+	}
+
+	private void grow()
+	{
+		// Extend the length of the branch.
+		// The segment before the tip ring will be extended. If it reaches its max length,
+		// then a new segment will be added, and the overflow growth distance
+		// will be its initial length.
+
+		float newGrowth = (lengthGoal - growthStart) * growthRate * Time.deltaTime;
+		int growIndex = skeleton.Count - 2;
+
+		// should probably throw an error if newGrowth > maxSegLength
+		if (newGrowth > maxSegLength)
+		{
+			Debug.Log("Whoops, newGrowth > maxSegLength in grow(). We should do something to handle this case.");
+		}
+
+		// trim the new growth if our vine is overshooting the total length goal
+		if (getLength() + newGrowth > lengthGoal)
+		{
+			newGrowth = lengthGoal - getLength();
+			growthStart = lengthGoal;
+		}
+
+		// if the only segment is the tip segment, then we need to start fresh on a new one.
+		if (skeleton.Count == 1)
+		{
+			addSegment(initialRadius, newGrowth, skeleton.Last().direction);
+			//Debug.Log("Creating first non-tip segment");
+		}
+		else
+		{
+			// we're not editing the very last segment, but the one right before it.
+			// this is to maintain constant tip length. Otherwise growth will look funky.
+			float currentLength = skeleton[growIndex].length;
+			float newSegLength = currentLength + newGrowth;
+
+			if (newSegLength < maxSegLength)
+			{
+				skeleton[growIndex].length = newSegLength;
+			}
+			else
+			{
+				skeleton[growIndex].length = maxSegLength;
+				float overflow = newSegLength - maxSegLength;
+				addSegment(initialRadius, overflow, skeleton.Last().direction);
+				//Debug.Log("Segment overflow (" + newSegLength + "). segment " + growIndex + " maxed out at " + skeleton[growIndex].length + ", so a new node is created with length " + overflow);
+			}
+		}
+
+		updateSkeleton(skeleton);
+	}
+
+	private void addSegment()
+	{
+		// steal from Vine
+	}
+
+	private void updateSkeleton()
+	{
+		// steal from Vine
 	}
 
 	public Branch getParent()
