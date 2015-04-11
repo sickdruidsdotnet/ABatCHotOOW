054a206d81ae5d248337fe669c211e630fcd320a
cdilday <cdilday@ucsc.edu>
2015-04-10 20:56:00 -0700

    Added seed selector files and better effects for smoother look and better performance

--- Amelia Blume/Assets/Scripts/Seed/SeedSelector.cs
+++ Amelia Blume/Assets/Scripts/Seed/SeedSelector.cs
@@ -8,6 +8,10 @@ public class SeedSelector : MonoBehaviour {
 	public GameObject stickImage;
 	GameObject player;
 	Renderer[] childRenderers;
+	public Renderer[] sections;
+	public SelectionEffect[] seeds;
+	string prevDirection;
+	string direction;
 
 	void Start(){
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -17,6 +21,8 @@ public class SeedSelector : MonoBehaviour {
 		childRenderers = GetComponentsInChildren<Renderer> ();
 		foreach(Renderer renderer in childRenderers)
 			renderer.enabled = false;
+		direction = "Up";
+		prevDirection = "Up";
 	}
 
 	// Update is called once per frame
@@ -30,9 +36,69 @@ public class SeedSelector : MonoBehaviour {
 		if (new Vector2 (horizontal2, vertical2).magnitude >= 0.3f) {
 			foreach(Renderer renderer in childRenderers)
 				renderer.enabled = true;
-			stickImage.transform.position = new Vector3 (transform.position.x + (horizontal2 / 3f),
-			                                   transform.position.y - (vertical2 / 3f),
-			                                   transform.position.z);
+			float angle = Vector2.Angle(Vector2.up * -1f, new Vector2(horizontal2, vertical2));
+			if( angle >= 45 && angle < 135 && horizontal2 > 0){
+				sections[0].enabled = false;
+				sections[2].enabled = false;
+				sections[3].enabled = false;
+				direction = "Right";
+			}
+			else if( angle >= 135 && angle <= 180){
+				sections[0].enabled = false;
+				sections[1].enabled = false;
+				sections[3].enabled = false;
+				direction = "Down";
+			}
+			else if( angle >= 45 && angle < 135 && horizontal2 < 0 ){
+				sections[0].enabled = false;
+				sections[2].enabled = false;
+				sections[1].enabled = false;
+				direction = "Left";
+			}
+			else{
+				sections[1].enabled = false;
+				sections[2].enabled = false;
+				sections[3].enabled = false;
+				direction = "Up";
+			}
+			stickImage.transform.position = new Vector3 (transform.position.x + (horizontal2 / 3.5f),
+			                                  			 transform.position.y - (vertical2 / 3.5f),
+			                                             stickImage.transform.position.z);
+
+			//let's get some cool rotation in here
+			if(prevDirection != direction){
+			switch (direction){
+				case "Up":
+					seeds[0].StartEffect();
+					break;
+				case "Right":
+					seeds[1].StartEffect();
+					break;
+				case "Down":
+					seeds[2].StartEffect();
+					break;
+				case "Left":
+					seeds[3].StartEffect();
+					break;
+				}
+
+				switch (prevDirection){
+				case "Up":
+					seeds[0].EndEffect();
+					break;
+				case "Right":
+					seeds[1].EndEffect();
+					break;
+				case "Down":
+					seeds[2].EndEffect();
+					break;
+				case "Left":
+					seeds[3].EndEffect();
+					break;
+				}
+			}
+			prevDirection = direction;
+
 		} else {
 			foreach(Renderer renderer in childRenderers)
 				renderer.enabled = false;
---
+++ Amelia Blume/Assets/SelectionEffect.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionEffect : MonoBehaviour {
+
+	bool active = false;
+	bool grow = false;
+	float startTime;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (active) {
+			if(grow)
+			{
+				transform.localScale = new Vector3(Mathf.SmoothStep(transform.localScale.x, 4.4f, (Time.time - startTime)/0.4f),
+				                                   Mathf.SmoothStep(transform.localScale.y, 4.4f, (Time.time - startTime)/0.4f),
+				                                   Mathf.SmoothStep(transform.localScale.z, 4.4f, (Time.time - startTime)/0.4f));
+			}
+			else{
+				transform.localScale = new Vector3(Mathf.SmoothStep(transform.localScale.x, 3f, (Time.time - startTime)/0.4f),
+				                                   Mathf.SmoothStep(transform.localScale.y, 3f, (Time.time - startTime)/0.4f),
+				                                   Mathf.SmoothStep(transform.localScale.z, 3f, (Time.time - startTime)/ 0.4f));
+				if(transform.localScale.x == 3)
+					active = false;
+			}
+		}
+	
+	}
+
+	public void StartEffect()
+	{
+		active = true;
+		grow = true;
+		startTime = Time.time;
+
+	}
+
+	public void EndEffect()
+	{
+		active = true;
+		grow = false;
+		startTime = Time.time;
+		
+	}
+}
