44f0fa02395e204bcc3b7fe521e985d2c8ce2df2
cdilday <cdilday@ucsc.edu>
2015-04-23 13:44:45 -0700

    - made a render optimization script. Attach it to any smaller object (Boar sized or less) and it will unrender it while not on screen - It's currently attached to a lot of prefabs - Probably finally fixed sun problem

--- Amelia Blume/Assets/Scripts/Core/RenderOptimizer.cs
+++ Amelia Blume/Assets/Scripts/Core/RenderOptimizer.cs
@@ -14,12 +14,17 @@ public class RenderOptimizer : MonoBehaviour {
 		mainCameraObject = GameObject.FindGameObjectWithTag ("MainCamera");
 		MeshRenderer[] childRenderers = gameObject.GetComponentsInChildren<MeshRenderer> ();
 		MeshRenderer currentRenderer = gameObject.GetComponent<MeshRenderer> ();
-		if (currentRenderer != null) {
+		if (currentRenderer != null && childRenderers[0] != null) {
 			allRenderers = new MeshRenderer[childRenderers.Length + 1];
 			childRenderers.CopyTo (allRenderers, 0);
 			allRenderers [childRenderers.Length - 1] = currentRenderer;
-		} else {
+		} else if(currentRenderer == null) {
 			allRenderers = childRenderers;
+		}
+		else
+		{
+			allRenderers = new MeshRenderer[1];
+			allRenderers[0] = currentRenderer;
 		}
 		rendered = true;
 
@@ -29,29 +34,34 @@ public class RenderOptimizer : MonoBehaviour {
 	void FixedUpdate () {
 		shouldRender = false;
 		foreach (MeshRenderer rend in allRenderers) {
+			if(rend == null)
+				break;
 			Bounds thisBounds = rend.bounds;
 			Vector3 checkPos = new Vector3(thisBounds.center.x + thisBounds.extents.x,
-			                               thisBounds.center.y, thisBounds.center.z);
+			                               thisBounds.center.y + thisBounds.extents.y,
+			                               thisBounds.center.z);
 			Vector2 camPosition = mainCameraObject.camera.WorldToViewportPoint (checkPos);
 			if (camPosition.x > 0 && camPosition.x < 1 && camPosition.y > 0 && camPosition.y < 1) {
 				shouldRender = true;
 				break;
 			}
-			checkPos = new Vector3(thisBounds.center.x - thisBounds.extents.x,
-			                       thisBounds.center.y, thisBounds.center.z);
+			checkPos = new Vector3(thisBounds.center.x + thisBounds.extents.x,
+			                       thisBounds.center.y - thisBounds.extents.y, thisBounds.center.z);
 			camPosition = mainCameraObject.camera.WorldToViewportPoint (checkPos);
 			if (camPosition.x > 0 && camPosition.x < 1 && camPosition.y > 0 && camPosition.y < 1) {
 				shouldRender = true;
 				break;
 			}
-			checkPos = new Vector3(thisBounds.center.x, thisBounds.center.y + thisBounds.extents.y, 
+			checkPos = new Vector3(thisBounds.center.x - thisBounds.extents.x,
+			                       thisBounds.center.y + thisBounds.extents.y, 
 			                       thisBounds.center.z);
 			camPosition = mainCameraObject.camera.WorldToViewportPoint (checkPos);
 			if (camPosition.x > 0 && camPosition.x < 1 && camPosition.y > 0 && camPosition.y < 1) {
 				shouldRender = true;
 				break;
 			}
-			checkPos = new Vector3(thisBounds.center.x, thisBounds.center.y - thisBounds.extents.y, 
+			checkPos = new Vector3(thisBounds.center.x - thisBounds.extents.x,
+			                       thisBounds.center.y - thisBounds.extents.y, 
 			                       thisBounds.center.z);
 			camPosition = mainCameraObject.camera.WorldToViewportPoint (checkPos);
 			if (camPosition.x > 0 && camPosition.x < 1 && camPosition.y > 0 && camPosition.y < 1) {
@@ -62,11 +72,15 @@ public class RenderOptimizer : MonoBehaviour {
 
 		if (shouldRender && !rendered) {
 			foreach (MeshRenderer rend in allRenderers) {
+				if(rend == null)
+					break;
 				rend.enabled = true;
 			}
 			rendered = true;
 		} else if (!shouldRender && rendered) {
 			foreach (MeshRenderer rend in allRenderers) {
+				if(rend == null)
+					break;
 				rend.enabled = false;
 			}
 			rendered = false;
