0ce0a70a6f31db169c407cb825234dbc8f93e8cf
cdilday <cdilday@ucsc.edu>
2015-04-30 13:32:19 -0700

    Camera Improvements and scene clean-up:
    - Added a dissapearer script ment for debug object to easily see when moving stuff around in editor
    - Switch focus points will no longer cause a full zoom in and out
    - Leafspawners now get deleted when they aren't spawning anything, for debug purposes
    - Focus points are now spheres when not running the game.

---
+++ Amelia Blume/Assets/dissapearer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dissapearer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// the point of this whole script is to shrink down things so they aren't
+		// viewable  in game but durting editing
+		transform.localScale = new Vector3(0.000001f, 0.000001f, 0.000001f);
+	}
+}
