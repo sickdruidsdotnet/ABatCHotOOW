730b5f50432368444c0c7c18ba0d649d2ba80220
smarredo <gam3r666@dhcp-169-233-163-163.cruznet.ucsc.edu>
2015-05-07 13:17:41 -0700

    Fixed Signpost?

--- Amelia Blume/Assets/Scripts/SignPost.cs
+++ Amelia Blume/Assets/Scripts/SignPost.cs
@@ -80,7 +80,7 @@ public class SignPost : MonoBehaviour {
 			DisplayWords ();
 			nextUse = Time.time + delay;
 		}
-
+		//Debug.Log(amelia.GetReadSign());
 		uiButtonSprite.enabled = uiText.enabled;
 		uiTextBoxSprite.enabled = uiText.enabled;
 		nameText.enabled = uiText.enabled;
@@ -140,11 +140,13 @@ public class SignPost : MonoBehaviour {
 				}
 				sentenceIndex++;
 			}
+			uiText.text = textDisplay;
+			nameText.text = speaker;
 		}
-		//myTextMesh.text = textDisplay;
-		uiText.text = textDisplay;
+		//Debug.Log (textDisplay);
+
 		//speaker.Trim(charsToTrim);
-		nameText.text = speaker;
+		//nameText.text = speaker;
 	}
 
 	void NextSentence(){
@@ -213,13 +215,22 @@ public class SignPost : MonoBehaviour {
 		}
 	}
 
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("enter");
+			textDisplay = "";
+		}
+	}
+
 	void OnTriggerExit(Collider other)
 	{
 		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Exit");
 			amelia.SetReadSign(false);
 			//Debug.Log ("stop reading");
-			beingRead = false;
 			textDisplay = "";
+			beingRead = false;
 			wordsIndex = 0;
 			//myTextMesh.renderer.enabled = false;
 			uiText.enabled = false;
