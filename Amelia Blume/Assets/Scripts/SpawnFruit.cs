42e2d8b0074daf484cbeb9f74f98efe0a73bf9a1
smarredo <gam3r666@Stephens-MacBook-Pro-2.local>
2015-05-17 19:12:20 -0700

    Player now needs to press Jump to spawn after dying

--- Amelia Blume/Assets/Scripts/SpawnFruit.cs
+++ Amelia Blume/Assets/Scripts/SpawnFruit.cs
@@ -2,25 +2,35 @@
 using System.Collections;
 
 public class SpawnFruit : MonoBehaviour {
+	GameObject amelia;
+	Player script;
+	PlayerController controller;
 
 	// Use this for initialization
 	void Start () {
+		amelia = GameObject.Find ("Player");
+		script = amelia.GetComponent<Player> ();
+		controller = amelia.GetComponent<PlayerController> ();
 		//ignore collisions with the player
         Physics.IgnoreCollision (GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider> (), transform.collider, true);
 		Physics.IgnoreCollision (GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController> (), transform.collider, true);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if (script.GetSpawn ()) {
+			this.GetComponent<Rigidbody>().useGravity = true;
+		} else {
+			this.GetComponent<Rigidbody>().useGravity = false;
+		}
 	}
 
 	void OnCollisionEnter(Collision other){
 		if (other.transform.tag != "Player") {
-			GameObject amelia = GameObject.Find ("Player");
-			Player script = amelia.GetComponent<Player> ();
+			script.SetSpawn(false);
 			script.SetCanGrow (true);
-			//Debug.Log ("Destroy fruit");
+			script.SetDead(false);
+			controller.canControl = true;
 			Destroy (this.gameObject);
 		}
 	}
