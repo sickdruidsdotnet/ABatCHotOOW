b4db0f012e7a063b0881ef1b6039a563566dcf07
cdilday <cdilday@ucsc.edu>
2015-05-23 19:12:59 -0700

    Creating a randomized fluff spawner that will automatically populate the level with fluff. Still needs some tweaks, but we will ideally be able to use this to generate backgrounds and even seed it if we want to keep it the same every time. Currently we can edit what objects can be spawned, the density at which to spawn them, the area where they will be spawned, and their spawn offset.

---
+++ Amelia Blume/Assets/Fluff_spawner.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fluff_spawner : MonoBehaviour {
+
+	public int preloadedSeed;
+	public GameObject[] fluffObjects;
+	//roughly how many gameObjects we want spawned in this area
+	public float density;
+
+	public float spawnOffset = 1.0f;
+
+	float spawnHeight;
+	float spawnDepth;
+	float xLeft;
+	float xRight;
+	float length;
+
+	// Use this for initialization
+	void Start () {
+		//seed random so we can get a consistant experience if we so chose
+		if (preloadedSeed == 0) {
+			Random.seed = (int) System.DateTime.Now.Ticks;
+		} else {
+			Random.seed = preloadedSeed;
+		}
+
+		if (density == 0) {
+			density = Random.Range(10, 30);
+		}
+
+		Bounds boxBounds = gameObject.GetComponent<BoxCollider> ().bounds;
+		spawnHeight = transform.position.y;
+		spawnDepth = transform.position.z; 
+		xLeft = transform.position.x - boxBounds.extents.x;
+		xRight = transform.position.x + boxBounds.extents.x;
+		length = Mathf.Abs (xLeft - xRight);
+
+		spawnFluff ();
+
+
+	}
+	
+	public void spawnFluff(){
+		if (fluffObjects.Length == 0) {
+			Debug.Log ("This instance of Fluff_spawner has no fluff in its array to spawn!");
+			return;
+		}
+
+		float roughDist = length / density;
+
+		Vector3 currPos = new Vector3 (xLeft, spawnHeight, spawnDepth);
+		while (currPos.x < xRight) {
+			Vector3 spawnPoint = new Vector3(Mathf.Clamp(currPos.x + Random.Range(-1 * spawnOffset, spawnOffset), xLeft, xRight),
+			                                 currPos.y, currPos.z);
+			/*if(spawnPoint.x > xRight)
+			{
+				break;
+			}*/
+			int index = Random.Range(0, fluffObjects.Length);
+			GameObject newFluff = Instantiate (fluffObjects[index], spawnPoint, fluffObjects[index].transform.rotation) as GameObject;
+			newFluff.transform.SetParent(transform);
+			currPos = new Vector3(spawnPoint.x + roughDist, currPos.y, currPos.z);
+		}
+
+	}
+}
