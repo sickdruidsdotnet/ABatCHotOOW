2e912e54f4fa1429e58aa1fa98418e9e934c279b
cdilday <cdilday@ucsc.edu>
2015-05-28 12:58:21 -0700

    Sun now targets plants with a ray when it's within 8 units

--- Amelia Blume/Assets/Scripts/Sun/Sun.cs
+++ Amelia Blume/Assets/Scripts/Sun/Sun.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Sun : MonoBehaviour {
 	private GameObject player;
 	public InputHandler playerInput;
+	public List<GameObject> targets;
 	// Use this for initialization
 	void Start () {
 		playerInput = GameObject.Find ("Input Handler").GetComponent<InputHandler> ();
 		player = GameObject.Find ("Player");
 		gameObject.name = "Sun";
+		//get all plants this will target
+		transform.position = new Vector3((player.transform.position.x +(0.5f* player.GetComponent<PlayerController>().faceDirection)),
+		                                 player.transform.position.y + 2f, transform.position.z);
+		targets = new List<GameObject> ();
+		GameObject[] allPlants = GameObject.FindGameObjectsWithTag ("Plant");
+		foreach (GameObject plant in allPlants) {
+			float dist = Vector3.Distance(transform.position, plant.transform.position);
+			if( dist <=8)
+			{
+				targets.Add (plant);
+			}
+		}
 
+		SunRay[] childRays = gameObject.GetComponentsInChildren<SunRay> ();
+		for (int i = 0; i < targets.Count; i++) {
+			childRays[i].target = targets[i];
+		}
 	}
 	
 	// Update is called once per frame
