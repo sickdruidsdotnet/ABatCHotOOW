4f6eb440aa6751b4c4357ee281a74bd42b0c709e
cdilday <cdilday@ucsc.edu>
2015-06-01 18:19:08 -0700

    Created a ground recoloring prefab that can overwrite all ground and ground fluff in a scene. does not currently look at hand-placed fluff or soil fluff yet

---
+++ Amelia Blume/Assets/Ground_Recolorer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground_Recolorer : MonoBehaviour {
+
+	public bool overwriteColors;
+	public Color grassColor;
+	public Color grassFluffColor;
+
+	// Use this for initialization
+	void Start () {
+		if (overwriteColors) {
+			GameObject[] allGround = GameObject.FindGameObjectsWithTag ("Ground");
+			GameObject[] allSoil = GameObject.FindGameObjectsWithTag ("Soil");
+
+			foreach (GameObject ground in allGround) {
+				//first recolor the fluff
+				Fluff_spawner[] fluff = ground.GetComponentsInChildren<Fluff_spawner>();
+				int i = 0;
+				foreach(Fluff_spawner spawner in fluff)
+				{
+					fluff[i].overrideColor = true;
+					fluff[i].newColor = grassFluffColor;
+					//fluff[i].respawnFluff();
+					i++;
+				}
+				//now find the ground and change the grass color
+				MeshRenderer render = ground.transform.Find("Ground").GetComponent<MeshRenderer>();
+				render.materials[1].color = grassColor;
+
+			}
+		}
+	}
+
+}
