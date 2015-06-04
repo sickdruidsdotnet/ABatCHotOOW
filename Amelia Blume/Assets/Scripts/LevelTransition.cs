7fe765b8cb4359db656a9b7d32bb4f1679ebafe0
Derek Donahue <propolandante@gmail.com>
2015-06-04 00:26:15 -0700

    Final level of of Act IV is functionally complete

--- Amelia Blume/Assets/Scripts/LevelTransition.cs
+++ Amelia Blume/Assets/Scripts/LevelTransition.cs
@@ -6,45 +6,53 @@ public class LevelTransition : MonoBehaviour {
 	GameObject gameController;
 	public int nextLevel;
 	public string nextLevelName;
-	public bool rightSide = true;
+	public bool right = true;
+	public bool left = false;
+	public bool up = false;
+	public bool down = false;
+
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
 		gameController = GameObject.FindGameObjectWithTag ("GameController");
+		if (left && right)
+		{
+			Debug.Log("LevelTransition triggers to left AND right. Should be one or the other!");
+		}
+		if (up && down)
+		{
+			Debug.Log("LevelTransition triggers to above AND below. Should be one or the other!");
+		}
+		if (!left && !right && !up && !down)
+		{
+			Debug.Log("LevelTransition will never trigger because none of the directions are true!");
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (rightSide && player.transform.position.x > this.transform.position.x) {
-			//check to see if highest level needs updating
-			if(PlayerPrefs.GetInt("Highest Stage", 0) < nextLevel)
-			{
-				PlayerPrefs.SetInt("Highest Stage", nextLevel);
-			}
-			//enter both the level num and name in order to keep it clear, but use the name for transitions
-			if(nextLevelName != null){
-				gameController.GetComponent<ABGameController>().BeginSceneTransition(nextLevelName);
-			}
-			else
-			{
-				gameController.GetComponent<ABGameController>().BeginSceneTransition(nextLevel);
-			}
+		if ((right && player.transform.position.x > this.transform.position.x)
+			|| (left && player.transform.position.x < this.transform.position.x)
+			|| (up && player.transform.position.y > this.transform.position.y)
+			|| (down && player.transform.position.y < this.transform.position.y)) 
+		{
+			transition();			
 		}
+	}
 
-		else if (!rightSide && player.transform.position.x < this.transform.position.x) {
-			//check to see if highest level needs updating
-			if(PlayerPrefs.GetInt("Highest Stage", 0) < nextLevel)
-			{
-				PlayerPrefs.SetInt("Highest Stage", nextLevel);
-			}
-			//enter both the level num and name in order to keep it clear, but use the name for transitions
-			if(nextLevelName != null){
-				gameController.GetComponent<ABGameController>().BeginSceneTransition(nextLevelName);
-			}
-			else
-			{
-				gameController.GetComponent<ABGameController>().BeginSceneTransition(nextLevel);
-			}
+	void transition()
+	{
+		if(PlayerPrefs.GetInt("Highest Stage", 0) < nextLevel)
+		{
+			PlayerPrefs.SetInt("Highest Stage", nextLevel);
+		}
+		//enter both the level num and name in order to keep it clear, but use the name for transitions
+		if(nextLevelName != null){
+			gameController.GetComponent<ABGameController>().BeginSceneTransition(nextLevelName);
+		}
+		else
+		{
+			gameController.GetComponent<ABGameController>().BeginSceneTransition(nextLevel);
 		}
 	}
 }
